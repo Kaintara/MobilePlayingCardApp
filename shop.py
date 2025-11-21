6087bca1013d2c64c0ebbd2351c9c1eb5d8e1cf4
Kaintara <barli2k@gmail.com>
2025-11-21 22:47:46 +0000

    Added some UI things to keep it fresh and keep me motivated.

--- shop.py
+++ shop.py
@@ -5,3 +5,30 @@ class Theme:
         theme.front_img = front
         theme.back_img = back
 
+class Shop:
+    def __init__(shop):
+        shop.inventory = []
+        shop.equipped = 'Default'
+        shop.unlocked_inventory = []
+        shop.coin_count = 0
+
+    def update(shop):
+        print("Shop has been updated!")
+
+    def equip_theme(shop,theme):
+        if theme.name in shop.unlocked_inventory:
+            shop.equipped = theme.name
+            shop.update()
+    
+    def buy_theme(shop,theme):
+        if theme.name not in shop.unlocked_inventory:
+            if theme.cost <= shop.coin_count:
+                shop.unlocked_inventory.append(theme.name)
+                shop.coin_count -= theme.cost
+                shop.update()
+            else:
+                pass
+                #Dialog = Shop_Dialog(theme.cost)
+                #Dialog.open()
+
+
