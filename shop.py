cef53c0cf863f6014b314245f1dd15c7c7254995
Kaintara <68016021+Kaintara@users.noreply.github.com>
2025-11-26 09:30:04 +0000

    Adding Card Assests for Card theme testing. You need to make your card themes or find good ones!

--- shop.py
+++ shop.py
@@ -5,3 +5,8 @@ class Theme:
         theme.front_img = front
         theme.back_img = back
 
+themeslst = ['Classic']
+
+def set_all_themes():
+    for theme in themeslst:
+        globals()[theme] = Theme(theme, 0, 'assets/img/theme1/king_of_clubs2.png', 'assets/img/theme1/card_back_red.png')
