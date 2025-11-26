3151a38305749051e610cf6c4aca4607e5649b43
Kaintara <68016021+Kaintara@users.noreply.github.com>
2025-11-26 12:29:56 +0000

    Making some progress on the Shop and Theme Classes

---
+++ save.py
@@ -0,0 +1,12 @@
+class SaveData():
+    def __init__(save):
+        save.alldata = {
+            'Shop' : {'equipped' : 'Classic',
+                     'unlocked_inventory' : ['Classic'],
+                     'coin_count' : 0, },
+            'App' : 
+            'Games' :
+        }
+
+    def savedata(save, app, threes, rummy, memory, shop):
+        
--- shop.py
+++ shop.py
@@ -8,16 +8,27 @@ class Theme:
 class Shop:
     def __init__(shop):
         shop.inventory = []
-        shop.equipped = 'Default'
-        shop.unlocked_inventory = []
+        shop.equipped = 'Classic'
+        shop.unlocked_inventory = ['Classic']
         shop.coin_count = 0
+
+    def set_all_themes(shop):
+        themeslst = ['Classic','Blue_Classic']
+        themes = shop.inventory
+        themes.append(Theme(themeslst[0], 0, 'assets/img/theme1/king_of_clubs2.png', 'assets/img/theme1/card_back_red.png'))
+        themes.append(Theme(themeslst[1], 10,'assets/img/theme2/Spades 13.png','assets/img/theme2/Back Blue 1.png'))
+
+    def get_theme(shop,theme_name):
+        for theme in shop.inventory:
+            if theme_name == theme.name:
+                return theme
 
     def update(shop):
         print("Shop has been updated!")
 
     def equip_theme(shop,theme):
-        if theme.name in shop.unlocked_inventory:
-            shop.equipped = theme.name
+        if theme in shop.unlocked_inventory:
+            shop.equipped = theme
             shop.update()
     
     def buy_theme(shop,theme):
@@ -31,8 +42,12 @@ class Shop:
                 #Dialog = Shop_Dialog(theme.cost)
                 #Dialog.open()
 
-
-
-def set_all_themes():
-    for theme in themeslst:
-        globals()[theme] = Theme(theme, 0, 'assets/img/theme1/king_of_clubs2.png', 'assets/img/theme1/card_back_red.png')
+ShopTest = Shop()
+ShopTest.set_all_themes()
+print(ShopTest.inventory[0].name,ShopTest.inventory[1].name)
+ShopTest.equip_theme('Blue_Classic')
+print(ShopTest.equipped)
+ShopTest.equipped = 'Blue_Classic'
+ShopTest.equip_theme('Classic')
+print(ShopTest.equipped)
+print(ShopTest.get_theme('Classic').cost)
