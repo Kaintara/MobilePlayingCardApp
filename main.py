9d698615736fe937ca52315cfbb26427f2f334c8
Marli <68016021+Kaintara@users.noreply.github.com>
2026-03-05 09:02:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+#Imports from Kivy Module
+
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.metrics import sp
+from kivy.metrics import dp
+from kivy.animation import Animation
+from kivy.graphics import Color, RoundedRectangle
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.graphics import PushMatrix, PopMatrix, Rotate, Scale, Translate
+from kivy.clock import Clock
+from kivy.uix.image import Image
+from kivy.core.audio import SoundLoader
+
+#Imports from KivyMD Module
+
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDButton, MDButtonIcon, MDIconButton, MDButtonText
+from kivymd.uix.behaviors import RotateBehavior
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.card import MDCard
+from kivymd.uix.relativelayout import RelativeLayout
+from kivymd.uix.label import MDLabel
+from kivymd.uix.gridlayout import GridLayout
+from kivymd.uix.fitimage import FitImage
+from kivymd.uix.dialog import MDDialog, MDDialogHeadlineText, MDDialogButtonContainer, MDDialogIcon, MDDialogContentContainer, MDDialogSupportingText
+
 #Imports
 from ui import * 
 from save import SaveData
